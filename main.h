130fdc78a31369726a0e3520b5bf9c0d497bbc85
danibalo <daniyemak@gmail.com>
2022-10-18 04:17:01 -0700

    produces output according to a format.

--- main.h
+++ main.h
@@ -1,22 +1,36 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
-#include <limits.h>
-#include <unistd.h>
-/**
- * struct format - match the conversion specifiers for printf
- * @ptr: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f:type pointer to function for the conversion specifier
- */
-typedef struct format
-{
-	char *ptr;
-	int (*f)();
-}convert match;
-int _putchar(char c);
-int _printf(const char *format, ...);
 
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
+ */
+typedef struct structprint
+{
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
+
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 #endif
